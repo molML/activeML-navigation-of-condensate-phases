83594ef262d3e1ea6902658eaf2e83c2a2d89050
AGardinon <andreagardin92@gmail.com>
2024-06-24 10:59:38 +0200

    reformatted the formats

--- robotexperiments/formats.py
+++ robotexperiments/formats.py
@@ -4,21 +4,24 @@ MASTER_FILE_NAME_FORMAT = 'master_file_version_{}.csv'
 
 MASTER_FILE_COLUMNS = [
     'Phase' ,
-    'conc_HEPES' ,
+    'Conc_HEPES' ,
     'pH' ,
-    'conc_Cy5_AP_Lys100' ,
-    'plate_barcode' ,
-    'plate_position' ,
-    'preparation_date' ,
-    'experiment_code' ,
-    'openT_protocol' ,
+    'Conc_Dye_Cy5_AP_Lys100_(mM)' ,
+    'Conc_Monomer_Cy5_AP_Lys100_(mM)' ,
+    'Plate_Barcode' ,
+    'Plate_Position' ,
+    'Preparation_Date' ,
+    'Experiment_Code' ,
+    'OpenT_Protocol' ,
     'Volume' ,
-    'barcode' ,
+    'Barcode' ,
 ]
 
+GIT_REPO_PATH = '/home/andreag/Work/1.main_project/git_repo/'
+
 FOLDERS_TREE = {
-    'master_file' : '/home/andreag/Work/1.main_project/git_repo/RobotLabExperiments/master_file/',
+    'master_file' : GIT_REPO_PATH+'RobotLabExperiments/master_file/',
     'experiments' : {
-        'robot001' : '/home/andreag/Work/1.main_project/git_repo/RobotLabExperiments/experiments/1.Asp200_Lys100_robot/',
+        'robot001' : GIT_REPO_PATH+'RobotLabExperiments/experiments/1.Asp200_Lys100_robot/',
     }
 }
