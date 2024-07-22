e0a7022ed6b6c66c006ef57216404335c23e22ea
AGardinon <andreagardin92@gmail.com>
2024-07-22 11:45:38 +0200

    added setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='robotexperiments',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/AGardinon/RobotLabExperiments',
+    author='Andrea Gardin',
+    description='RobotLab Experiment package.',
+)
