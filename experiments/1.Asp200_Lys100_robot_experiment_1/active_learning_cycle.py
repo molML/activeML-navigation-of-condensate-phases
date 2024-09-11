c156d2c7da4e2c031da5282ff71686dfc911f718
AGardinon <andreagardin92@gmail.com>
2024-09-11 17:19:07 +0200

    added option to atumatically extract validated points

--- experiments/1.Asp200_Lys100_robot_experiment_1/active_learning_cycle.py
+++ experiments/1.Asp200_Lys100_robot_experiment_1/active_learning_cycle.py
@@ -6,7 +6,7 @@
 # -------------------------------------------------- #
 
 import pprint
-from robotexperiments.cycle import active_cycle, append_to_masterfile
+from robotexperiments.cycle import active_cycle, append_to_masterfile, extract_validated_points
 
 # --- Variables
 
@@ -27,7 +27,7 @@ experiment_cycle_dict_tmp = {
     'experimentID': experimentID,
     'cycle_number' : cycle_number_tmp,
     'search_space_dataset': 'DOE_Asp200_Lys100_robotExp1_3Dim.csv',
-    'validated_dataset' : None, #f'{experimentID}_cycle{cycle_number_tmp-1}_validated_points.csv', # or None
+    'validated_dataset' : f'{experimentID}_cycle{cycle_number_tmp-1}_validated_points.csv',
     'new_points_batch' : new_points_batch_tmp,
     'classifier_model' : 'GaussianProcessClassifier',
     'classifier_dict': classifier_dict_tmp,
@@ -47,6 +47,14 @@ if __name__ == '__main__':
 
     print('Evaluation Config:')
     pprint.pprint(experiment_cycle_dict_tmp)
+
+    # if cycle is 0, the validate points are None
+    if cycle_number_tmp == 0:
+        experiment_cycle_dict_tmp['validated_dataset'] = None
+
+    # if the cycle is > 0 it will extract the validated points from the cycle-1
+    elif experiment_cycle_dict_tmp['cycle_number'] > 0:
+        extract_validated_points(experimentID=experimentID, cycle_number=cycle_number_tmp-1)
 
     # - do the cycle
     active_cycle(**experiment_cycle_dict_tmp)
