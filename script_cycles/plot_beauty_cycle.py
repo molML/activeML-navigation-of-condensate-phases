fa79be992cb079e318c697796d093bafa5762c63
AGardinon <andreagardin92@gmail.com>
2024-09-27 10:51:35 +0200

    revert to previous plotting style for the original file

--- script_cycles/plot_beauty_cycle.py
+++ script_cycles/plot_beauty_cycle.py
@@ -5,10 +5,8 @@
 # AUTHOR: Andrea Gardin
 # -------------------------------------------------- #
 
-import joblib
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import robotexperiments.beauty as beauty
 from robotexperiments.formats import FOLDERS_TREE
@@ -25,11 +23,10 @@ var3 = 'Conc_NaCl_(mM)'
 xyvar = (var1,var2)
 zvar = var3
 
-algorithm_file_format = '/cycles/cycle_{}/'+experimentID+'_cycle{}_output_algorithm.pkl'
+pdf_file_template = '/cycles/cycle_{}/'+experimentID+'_cycle{}_output_points_pdf.npy'
+new_points_ndx_format = '/cycles/cycle_{}/'+experimentID+'_cycle{}_output_points.ndx'
 
-output_points_format = '/cycles/cycle_{}/'+experimentID+'_cycle{}_output_points.csv'
-
-cycle_number_tmp = 3
+cycle_number_tmp = 6
 
 # --- main
 
@@ -37,95 +34,52 @@ if __name__ == '__main__':
 
     EXPdir = FOLDERS_TREE['experiments'][experimentID]
     df = pd.read_csv(EXPdir+search_space_dataset)
-    
-    matrix = df.drop(columns='Phase')
-    matrix = StandardScaler().fit_transform(matrix)
 
     zvar_value_list = np.unique(df[zvar].values)
-    print(zvar_value_list)
+
+    pdf = np.load(EXPdir+pdf_file_template.format(cycle_number_tmp,cycle_number_tmp))
 
     X,Y = xyvar
     Z = zvar
 
     screened_points_ndx = []
-
-    for i in range(3,cycle_number_tmp+1):
-
-        if i == 0:
-            # screened_points = pd.read_csv(EXPdir+output_points_format.format(i,i))
+    for i in range(cycle_number_tmp):
+        try:
+            previous_points_ndx = np.loadtxt(EXPdir+new_points_ndx_format.format(i, i)).astype(int)
+            screened_points_ndx += list(previous_points_ndx)
+        except:
             pass
 
-        elif i > 0:
-            
-            model = joblib.load(EXPdir+algorithm_file_format.format(i,i))
-            pdf = model.predict_proba(matrix)
+    new_points_ndx = np.loadtxt(EXPdir+new_points_ndx_format.format(cycle_number_tmp, cycle_number_tmp)).astype(int)
 
-            fig, ax = beauty.get_axes(len(zvar_value_list),6)
-            fig1, ax1 = beauty.get_axes(len(zvar_value_list),6)
-
-            for j,zv in enumerate(zvar_value_list[:]):
-
-                beauty.plot2D_3variable_pdfsurface(df=df,
-                                                pdf=pdf,
-                                                var1=X,
-                                                var2=Y,
-                                                constant_var=(zvar,zv),
-                                                screened_points_ndx=None,
-                                                new_points_ndx=None,
-                                                show_points=False,
-                                                axis=ax[j])
-
-                beauty.plot2D_3variable_entropysurface(df=df,
-                                                pdf=pdf,
-                                                var1=X,
-                                                var2=Y,
-                                                constant_var=(zvar,zv),
-                                                screened_points_ndx=None,
-                                                new_points_ndx=None,
-                                                show_points=False,
-                                                axis=ax1[j])
-
-    fig.tight_layout()
-    fig.savefig('test.png')
-    fig1.tight_layout()
-    fig1.savefig('test1.png')
-
-    #     try:
-    #         previous_points_ndx = np.loadtxt(EXPdir+new_points_ndx_format.format(i, i)).astype(int)
-    #         screened_points_ndx += list(previous_points_ndx)
-    #     except:
-    #         pass
-
-    # new_points_ndx = np.loadtxt(EXPdir+new_points_ndx_format.format(cycle_number_tmp, cycle_number_tmp)).astype(int)
-
-    # for zv in zvar_value_list[:]:
-    #     fig, ax = beauty.get_axes(3,3)
-    #     beauty.plot2D_3variable_points(df=df,
-    #                                    var1=X,
-    #                                    var2=Y,
-    #                                    constant_var=(zvar,zv),
-    #                                    screened_points_ndx=screened_points_ndx,
-    #                                    new_points_ndx=new_points_ndx,
-    #                                    axis=ax[0])
+    for zv in zvar_value_list[:]:
+        fig, ax = beauty.get_axes(3,3)
+        beauty.plot2D_3variable_points(df=df,
+                                       var1=X,
+                                       var2=Y,
+                                       constant_var=(zvar,zv),
+                                       screened_points_ndx=screened_points_ndx,
+                                       new_points_ndx=new_points_ndx,
+                                       axis=ax[0])
         
-    #     beauty.plot2D_3variable_pdfsurface(df=df,
-    #                                        pdf=pdf,
-    #                                        var1=X,
-    #                                        var2=Y,
-    #                                        constant_var=(zvar,zv),
-    #                                        screened_points_ndx=screened_points_ndx,
-    #                                        new_points_ndx=new_points_ndx,
-    #                                        show_points=True,
-    #                                        axis=ax[1])
+        beauty.plot2D_3variable_pdfsurface(df=df,
+                                           pdf=pdf,
+                                           var1=X,
+                                           var2=Y,
+                                           constant_var=(zvar,zv),
+                                           screened_points_ndx=screened_points_ndx,
+                                           new_points_ndx=new_points_ndx,
+                                           show_points=True,
+                                           axis=ax[1])
 
-    #     beauty.plot2D_3variable_entropysurface(df=df,
-    #                                            pdf=pdf,
-    #                                            var1=X,
-    #                                            var2=Y,
-    #                                            constant_var=(zvar,zv),
-    #                                            screened_points_ndx=screened_points_ndx,
-    #                                            new_points_ndx=new_points_ndx,
-    #                                            show_points=True,
-    #                                            axis=ax[2])
-    #     fig.tight_layout()
-    #     plt.show()
+        beauty.plot2D_3variable_entropysurface(df=df,
+                                               pdf=pdf,
+                                               var1=X,
+                                               var2=Y,
+                                               constant_var=(zvar,zv),
+                                               screened_points_ndx=screened_points_ndx,
+                                               new_points_ndx=new_points_ndx,
+                                               show_points=True,
+                                               axis=ax[2])
+        fig.tight_layout()
+        plt.show()
