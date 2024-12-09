8bdcae863271d9c9da30e446a331f6874ffbf82d
AGardinon <andreagardin92@gmail.com>
2024-12-09 13:54:58 +0100

    uniformed setup.py to new repo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='robotexperiments',
     version='1.1.0',
     packages=find_packages(),
-    url='https://github.com/AGardinon/RobotLabExperiments',
+    url='https://github.com/molML/activeML-navigation-of-condensate-phases',
     author='Andrea Gardin',
-    description='RobotLab Experiment package.',
+    description='Python package for the automated navigation of condensate phase behavior with active machine learning',
 )
