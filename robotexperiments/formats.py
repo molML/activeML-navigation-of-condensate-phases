7c92b64d9b292575489df9767e7c7b9ac470ec05
AGardinon <andreagardin92@gmail.com>
2025-04-10 11:20:34 +0200

    added new insilico experiments

--- robotexperiments/formats.py
+++ robotexperiments/formats.py
@@ -1,7 +1,7 @@
 #!
 import os
 
-MASTER_FILE_NAME_FORMAT = 'master_file_version_invitro_{}.csv'
+MASTER_FILE_NAME_FORMAT = 'master_file_version_insilico_{}.csv'
 
 TARGET_LABEL = 'Phase'
 UNASSIGNED_TARGET_VALUES = -1
@@ -29,7 +29,12 @@ FOLDERS_TREE = {
         'RoboLab007_Lys100_Asp200_150mM_NaCl_2Dmatrix_1decimal_R3' : EXP_REPO_PATH+'/RoboLab007_Lys100_Asp200_150mM_NaCl_2Dmatrix_1decimal_R3/',
         'RoboLab008_Lys100_Asp200_NaCl_Matrix_R1' : EXP_REPO_PATH+'/RoboLab008_Lys100_Asp200_NaCl_Matrix_R1/',
         'RoboLab008_Lys100_Asp200_NaCl_Matrix_R2' : EXP_REPO_PATH+'/RoboLab008_Lys100_Asp200_NaCl_Matrix_R2/',
-        'RoboLab008_Lys100_Asp200_NaCl_Matrix_insilico_R1' : EXP_REPO_PATH+'/RoboLab008_Lys100_Asp200_NaCl_Matrix_insilico_R1/'
+        'RoboLab008_Lys100_Asp200_NaCl_Matrix_insilico_R1' : EXP_REPO_PATH+'/RoboLab008_Lys100_Asp200_NaCl_Matrix_insilico_R1/',
+        'RoboLab007_Lys100_Asp200_150mM_NaCl_2Dmatrix_1decimal_insilico_R1' : EXP_REPO_PATH+'/RoboLab007_Lys100_Asp200_150mM_NaCl_2Dmatrix_1decimal_insilico_R1/',
+        'RoboLab007_Lys100_Asp200_150mM_NaCl_2Dmatrix_1decimal_insilico_R2' : EXP_REPO_PATH+'/RoboLab007_Lys100_Asp200_150mM_NaCl_2Dmatrix_1decimal_insilico_R2/',
+        'RoboLab007_Lys100_Asp200_150mM_NaCl_2Dmatrix_1decimal_insilico_R3' : EXP_REPO_PATH+'/RoboLab007_Lys100_Asp200_150mM_NaCl_2Dmatrix_1decimal_insilico_R3/',
+        'RoboLab008_Lys100_Asp200_NaCl_Matrix_insilico_R2' : EXP_REPO_PATH+'/RoboLab008_Lys100_Asp200_NaCl_Matrix_insilico_R2/',
+        'RoboLab008_Lys100_Asp200_NaCl_Matrix_insilico_R3' : EXP_REPO_PATH+'/RoboLab008_Lys100_Asp200_NaCl_Matrix_insilico_R3/',
     }
 }
 
